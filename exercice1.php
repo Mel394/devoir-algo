f509fcabe07650cd7561b64484eb4b54753f0ae0
Mel394 <melanie.rose142@gmail.com>
2024-07-26 16:27:39 +0200

    final

--- exercice1.php
+++ exercice1.php
@@ -7,9 +7,7 @@ echo "$budgetinitial \n";
 
 //créer une boucle qui continuera tant que le budget initial n'est pas nul
 
-
-while($budgetinitial >= 0)
-echo "$budgetinitial" . "\n";
+$somme = $budgetinitial
 
 
 //créer une boucle qui continuera tant que le budget initial n'est pas nul, dans chaque itération de la boucle, demander à l'utilisateur de choisir entre une recette (R) ou une dépense (D) selon le choix : pour cette recette: demander le montant et ajoutez le au budget, pour une dépense demander le montant, vérifier si la dépense est possible par rapport au solde disponible. si possible : calculer et afficher le poucentage qui représente cette dépense par rapport au budget initial, retirer la dispense du budget. Si impossible: afficher un message d'erreur, afficher le budget disponible après chaque opération. Si le budget devient nul ou négatif, arrêtez le programme.
@@ -34,12 +32,18 @@ while($budgetinitial >= 0)
         {
             $budgetinitial -= $montant;
             $pourcentage = ($montant / $budgetinitial) * 100;
-            echo "Votre dépense de $montant représente $
-            pourcentage% du budget initial.\n";
+            echo "Votre dépense de $montant représente $pourcentage du budget initial.\n";
         }
 
     }
-}  
+  
+     echo "Votre budget est maintenant $budgetinitial.\n";
+    if($budgetinitial <= 0)
+    {
+        echo "Votre budget est nul ou négatif. Le programme est arrêté.\n";
+    }
+
+}
 
 
 
