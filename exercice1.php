4e2f919e30382f4e4610fe497e2ecc6737f1bc44
Mel394 <melanie.rose142@gmail.com>
2024-07-26 14:52:56 +0200

    budget initial

--- exercice1.php
+++ exercice1.php
@@ -1,1 +1,10 @@
-hihihi
+<?php 
+// creer un programme php qui permet à l'utilisateur de gérer un budget en effectuant des opérations de recettes et de dépenses
+
+//Demannder à l'utilisateur de saisir sont budget initial
+
+$budgetinitial =readline("Quel est votre budget initial?");
+echo "$budgetinitial \n"; 
+
+
+?>
