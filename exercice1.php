49b9887c7808699a0a427f58d1d374dde7056755
Mel394 <melanie.rose142@gmail.com>
2024-07-26 15:13:59 +0200

    partie2

--- exercice1.php
+++ exercice1.php
@@ -1,11 +1,16 @@
 <?php 
-// creer un programme php qui permet à l'utilisateur de gérer un budget en effectuant des opérations de recettes et de dépenses
-
 //Demannder à l'utilisateur de saisir sont budget initial
 
 $budgetinitial =readline("Quel est votre budget initial?");
 echo "$budgetinitial \n"; 
 
-//crée
+//créer une boucle qui continuera tant que le budget initial n'est pas nul
+
+for( $i=-1; $i<=$budgetinitial; $i--)
+{ echo $budgetinitial . "\n";
+}
+
+
+
 
 ?>
